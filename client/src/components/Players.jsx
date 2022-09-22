c5e5c70ef31b1febec16deb1a7c082a8b5c85ad2
Dinko99 <dinkodin99@gmail.com>
2022-09-22 03:21:31 +0200

    Finished search component for players

--- client/src/components/Players.jsx
+++ client/src/components/Players.jsx
@@ -1,11 +1,33 @@
 import { FiSearch } from 'react-icons/fi';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { atpSearchOptions } from '../utils/fetchData';
 
 const Players = () => {
   const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
 
-  const handleSearch = (e) => {
-    setSearch(e.target.value);
+  const fetchPlayersSearch = () => {
+    fetch(
+      `https://tennis-live-data.p.rapidapi.com/players/ATP`,
+      atpSearchOptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        const result = response.results.players;
+        setResults(result);
+        // console.log(result[0].full_name.replace(/\s/g, ''));
+      })
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    fetchPlayersSearch();
+  }, [query]);
+
+  const handleSearch = (q) => {
+    setSearch(q);
+    setQuery(q);
   };
 
   return (
@@ -13,14 +35,33 @@ const Players = () => {
       <form onSubmit={(e) => e.preventDefault()}>
         <input
           type='text'
+          autoFocus
           placeholder='Search For Players'
-          onChange={handleSearch}
+          value={search}
+          onChange={(e) => handleSearch(e.target.value)}
         ></input>
         <div className='search-button-bcg'>
           <FiSearch className='search-button' />
         </div>
       </form>
-      <p>{search}</p>
+      <div className='searched-players'>
+        {results.map((player) => {
+          const fullName = player.full_name.toLowerCase().replace(/\s/g, '');
+          if (query === '' || query.length <= 2) {
+            return null;
+          } else if (
+            fullName.includes(query.toLowerCase().replace(/\s/g, ''))
+          ) {
+            return (
+              <a href='#' key={player.id}>
+                {player.full_name}
+              </a>
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>
     </div>
   );
 };
--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const atpOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '537db61a16msh11ac12b8966c6aap1f23bajsna362049f725e',
+    'X-RapidAPI-Key': 'a883a7afb4msh27efc01a7397825p14b639jsnf54856c2d457',
     'X-RapidAPI-Host': 'tennis-live-data.p.rapidapi.com',
   },
 };
@@ -11,5 +11,13 @@ export const tournamentOptions = {
   headers: {
     'X-RapidAPI-Key': '537db61a16msh11ac12b8966c6aap1f23bajsna362049f725e',
     'X-RapidAPI-Host': 'ultimate-tennis1.p.rapidapi.com',
+  },
+};
+
+export const atpSearchOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'a883a7afb4msh27efc01a7397825p14b639jsnf54856c2d457',
+    'X-RapidAPI-Host': 'tennis-live-data.p.rapidapi.com',
   },
 };
